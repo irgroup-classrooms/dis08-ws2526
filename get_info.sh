d1e95b9942744fa6003bb34c0e7dd77d7cb1cf3e
Joann Kleinheyer <joann_kylee.kleinheyer@smail.th-koeln.de>
2026-01-16 00:01:25 +0100

    Add command to count lines in each TSV file

--- get_info.sh
+++ get_info.sh
@@ -4,3 +4,9 @@ set -euo pipefail
 echo "Task 1: first three lines of 2014-01_JA.tsv"
 head -n 3 /Users/joannkleinheyer/Desktop/Uni/DIS08/lab4/shell-lesson/2014-01_JA.tsv 
 
+echo
+echo "Task 2: line count of each TSV"
+wc -l /Users/joannkleinheyer/Desktop/Uni/DIS08/lab4/shell-lesson/*.tsv
+
+
+
